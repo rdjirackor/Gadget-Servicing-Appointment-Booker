89641f72cf699f3f86dc3a1e83a8fae82ebdac2e
Jum-eng <jmusah007@st.ug.edu.gh>
2024-07-16 23:37:16 +0000

    updated HomePage

--- gadget-servicing-appointment-booker/src/pages/Homepage.jsx
+++ gadget-servicing-appointment-booker/src/pages/Homepage.jsx
@@ -1,20 +1,20 @@
-import {React, useEffect} from 'react';
+import React, { useEffect } from 'react';
 import surface from '../assets/surface.png';
 
-function Homepage(){
+function Homepage() {
   useEffect(() => {
     document.title = "Book an Appointment"; // Set the new page title
   }, []);
 
-  return(
- <div className='body'>
-  <div className='welcome-banner'>
-    <div className='image-with-intro'>
-      <img src={surface}></img>
+  return (
+    <div className='body'>
+      <div className='welcome-banner'>
+        <div className='image-with-intro'>
+          <img src={surface} alt="Surface" />
+        </div>
+      </div>
     </div>
-  </div>
-
- </div>
-)};
+  );
+}
 
 export default Homepage;
