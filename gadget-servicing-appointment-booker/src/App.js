13032ba1fb95d2cc8ba2b26a7c24e3b781bacebd
Richard Djirackor <rdjirackor@st.ug.edu.gh>
2024-07-17 13:56:02 +0000

    Fixed routes problem

--- gadget-servicing-appointment-booker/src/App.js
+++ gadget-servicing-appointment-booker/src/App.js
@@ -1,32 +1,28 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Homepage from './pages/Homepage';
-import About from './pages/About';
+import About from './pages/About.jsx';
 import Pages from './pages/Pages';
 import Services from './pages/Services';
 import Appointment from './pages/Appointment';
 import Contact from './pages/Contact';
 
-
-
-
-
 function App() {
-  return(
+  return (
     <div className='App'>
-        <Router>
-            <Navbar />
-            <Switch>
-                <Route path='/' exact Component={Homepage}/>
-                <Route path='/about' exact Component={About}/>
-                <Route path='/pages' exact Component={Pages}/>
-                <Route path='/services' exact Component={Services}/>
-                <Route path='/appointment' exact Component={Appointment}/>
-                <Route path='/contact' exact Component={Contact}/>
-            </Switch>
-        </Router>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Homepage} />
+          <Route path='/about' exact component={About} />
+          <Route path='/pages' exact component={Pages} />
+          <Route path='/services' exact component={Services} />
+          <Route path='/appointment' exact component={Appointment} />
+          <Route path='/contact' exact component={Contact} />
+        </Switch>
+      </Router>
     </div>
-);
+  );
 }
 
 export default App;
--- gadget-servicing-appointment-booker/src/pages/About.jsx
+++ gadget-servicing-appointment-booker/src/pages/About.jsx
@@ -1,7 +1,7 @@
 const About = () => {
     return (
         <div className="about">
-            
+            <h1>Hello World!</h1>
         </div>
     );
 }
